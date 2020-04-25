8cc924162465d8d578fb561dcdb0358a21678d55
EvelynCV <28535160+EvelynCV@users.noreply.github.com>
2020-04-25 04:40:36 -0500

    Diseño de botones y seleccionador de Instituciones
    Reestructuración de botones de presentación de graficos y diseño de seleccionador de instituciones

--- WebServer/api-web/Publico/js/selector.js
+++ WebServer/api-web/Publico/js/selector.js
@@ -1,13 +1,43 @@
-$(document).ready(function() {
+const inputField = document.querySelector('.chosen-value');
+const dropdown = document.querySelector('.value-list');
+const dropdownArray = [...document.querySelectorAll('li')];
+console.log(typeof dropdownArray);
+dropdown.classList.add('open');
+inputField.focus(); // Demo purposes only
+let valueArray = [];
+dropdownArray.forEach(item => {
+    valueArray.push(item.textContent);
+});
 
-    $(".selLabel").click(function () {
-        $('.dropdown').toggleClass('active');
+const closeDropdown = () => {
+    dropdown.classList.remove('open');
+};
+
+dropdownArray.forEach(item => {
+    item.addEventListener('click', evt => {
+        inputField.value = item.textContent;
+        dropdownArray.forEach(dropdown => {
+            dropdown.classList.add('closed');
+        });
     });
+});
 
-    $(".dropdown-list li").click(function() {
-        $('.selLabel').text($(this).text());
-        $('.dropdown').removeClass('active');
-        $('.selected-item p span').text($('.selLabel').text());
+inputField.addEventListener('focus', () => {
+    dropdown.classList.add('open');
+    dropdownArray.forEach(dropdown => {
+        dropdown.classList.remove('closed');
     });
+});
 
+inputField.addEventListener('blur', () => {
+    inputField.placeholder = 'Seleccione una capa';
+    dropdown.classList.remove('open');
+});
+
+document.addEventListener('click', evt => {
+    const isDropdown = dropdown.contains(evt.target);
+    const isInput = inputField.contains(evt.target);
+    if (!isDropdown && !isInput) {
+        dropdown.classList.remove('open');
+    }
 });
